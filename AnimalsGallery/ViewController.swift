e47e83e651eee6c9de642f16cc714d930a19de9e
Dawid <dawidlabno@outlook.com>
2023-01-15 18:49:26 +0100

    Initial Commit

---
+++ AnimalsGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalsGallery
+//
+//  Created by Dawid Łabno on 15/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
